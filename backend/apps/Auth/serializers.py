ac7df21e0e87b035835f270063cfc1375a0e774e
Sebastian <se6359@gmail.com>
2022-11-07 02:51:51 +0100

    user

--- backend/apps/Auth/serializers.py
+++ backend/apps/Auth/serializers.py
@@ -2,9 +2,12 @@ from .models import User
 from rest_framework import serializers
 
 class SignUpSerializer(serializers.ModelSerializer):
+    image = serializers.CharField(source='skp.image')
+    name = serializers.CharField(source='skp.name')
+    city = serializers.CharField(source='skp.city')
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'password']
+        fields = ['first_name', 'last_name', 'email', 'password', 'name']
         extra_kwargs = {
             'password': {'write_only': True}
         }
