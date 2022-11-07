75a4d644dc8b25fd49f28b66e1ccace1204f27d2
Sebastian <se6359@gmail.com>
2022-11-07 03:07:20 +0100

    images

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path('images/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('', include('apps.Auth.urls')),
     path('', views.index),
     path('skp', views.index),
