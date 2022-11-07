92f5ad3dae6caeb9a99820c23c9912131d66a33d
Sebastian <se6359@gmail.com>
2022-11-07 03:08:14 +0100

    images

--- backend/apps/Auth/models.py
+++ backend/apps/Auth/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class SKP(models.Model):
-    image = models.ImageField(upload_to='SKP')
+    image = models.ImageField(upload_to='skp')
     name = models.CharField(max_length=255, unique=True)
     city = models.CharField(max_length=255)
     is_verified = models.BooleanField(default=False)
