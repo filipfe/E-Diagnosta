74f00da1bd8592aa5eaa7dbd7675ebbc3672dc4c
Filip <filfer05@gmail.com>
2022-11-14 02:53:35 +0100

    cities fix

--- frontend/src/components/skp/StationFilter.tsx
+++ frontend/src/components/skp/StationFilter.tsx
@@ -32,9 +32,9 @@ export default function SKPFilter({ setInput, setFilter, filter }: FilterProps) 
             <div className="flex items-center gap-4">
                 <h4 className="font-semibold">Miasto: </h4>
                 <div className="relative">
-                    <button className="bg-primary text-sm py-3 px-6 rounded font-medium text-white" onClick={() => setActive(prev => { return { ...prev, cities: !prev.cities }})}>{filter.city ? filter.city : 'Wybierz miasto'}</button>
+                    <button className="bg-primary text-sm py-3 px-6 rounded hover:bg-darkPrimary font-semibold text-white" onClick={() => setActive(prev => { return { ...prev, cities: !prev.cities }})}>{filter.city ? filter.city : 'Wybierz miasto'}</button>
                     {active.cities && <ul className="flex flex-col absolute top-[120%] z-10 w-max min-h-max max-h-[1.5in] overflow-y-auto rounded-xl overflow-hidden right-0 shadow-primarySmall">
-                        {allFilters.cities.length > 0 ? allFilters.cities.map(city => <li className="py-3 px-6 hover:bg-[#E4E4E4] transition-colors bg-white cursor-pointer" onClick={() => setFilter((prev: {}) => { return { ...prev, city: city}})}>{city}</li>) : <Loader className="mx-auto my-6 w-6" />}
+                        {allFilters.cities.length > 0 ? allFilters.cities.map(city => <li className="py-3 px-6 pr-10 hover:bg-[#E4E4E4] transition-colors bg-white cursor-pointer" onClick={() => setFilter((prev: {}) => { return { ...prev, city: city}})}>{city}</li>) : <Loader className="mx-auto my-6 w-6" />}
                     </ul>}
                 </div>
             </div>
